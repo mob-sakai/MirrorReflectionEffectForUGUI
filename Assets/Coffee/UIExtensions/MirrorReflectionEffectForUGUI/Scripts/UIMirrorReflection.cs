6c7c21ed8afbfb9a1f2fc095715d3a5bd6c5953e
mob-sakai <sakai@mobcast.jp>
2018-06-11 16:13:38 +0900

    ...

--- Assets/Coffee/UIExtensions/MirrorReflectionEffectForUGUI/Scripts/UIMirrorReflection.cs
+++ Assets/Coffee/UIExtensions/MirrorReflectionEffectForUGUI/Scripts/UIMirrorReflection.cs
@@ -6,6 +6,7 @@ using Coffee.UIExtensions;
 
 namespace Coffee.UIExtensions
 {
+	[ExecuteInEditMode]
 	public class UIMirrorReflection : BaseMeshEffect
 	{
 		//################################
@@ -145,6 +146,12 @@ namespace Coffee.UIExtensions
 		//################################
 		Rect _rect;
 
+		void Update()
+		{
+			graphic.SetVerticesDirty ();
+		}
+
+
 		void AddMirrorReflectedQuad(UIVertex[] quad, List<UIVertex> result)
 		{
 			// Read the existing quad vertices
@@ -152,6 +159,81 @@ namespace Coffee.UIExtensions
 			UIVertex v1 = quad[1];	// top-left
 			UIVertex v2 = quad[2];	// top-right
 			UIVertex v3 = quad[3];	// bottom-right
+
+			var t = graphic.rectTransform;
+//			var matrix =Matrix4x4.TRS (Vector3.zero, Quaternion.identity, Vector3.one);
+//			var matrix = Matrix4x4.TRS (new Vector3(0, -_rect.height-m_Spacing), Quaternion.identity, new Vector3(1,-1,1));
+
+			var lm = Matrix4x4.TRS (t.localPosition, t.localRotation, t.localScale);
+
+			var matrix = Matrix4x4.TRS (Vector3.zero, Quaternion.identity, new Vector3(1,-1,1));
+
+			matrix = Matrix4x4.TRS (new Vector3(0, -_rect.height-m_Spacing), Quaternion.identity, new Vector3(1,-1,1));
+
+			var rot = Quaternion.Euler (-t.parent.rotation.eulerAngles);
+			var rot2 = Quaternion.Euler (-t.localRotation.eulerAngles);
+			var rot3 = Quaternion.Euler (t.localRotation.eulerAngles);
+
+			var matrix2 =Matrix4x4.TRS (Vector3.zero, t.rotation, Vector3.one);
+
+
+//			quad[0].position = matrix2.MultiplyPoint3x4(rot * matrix.MultiplyPoint3x4((quad[0].position)));
+//			quad[1].position = matrix2.MultiplyPoint3x4(rot * matrix.MultiplyPoint3x4((quad[1].position)));
+//			quad[2].position = matrix2.MultiplyPoint3x4(rot * matrix.MultiplyPoint3x4((quad[2].position)));
+//			quad[3].position = matrix2.MultiplyPoint3x4(rot * matrix.MultiplyPoint3x4((quad[3].position)));
+
+//			quad[0].position = (matrix.MultiplyPoint3x4(rot2*rot * (quad[0].position)));
+//			quad[1].position = (matrix.MultiplyPoint3x4(rot2*rot * (quad[1].position)));
+//			quad[2].position = (matrix.MultiplyPoint3x4(rot2*rot * (quad[2].position)));
+//			quad[3].position = (matrix.MultiplyPoint3x4(rot2*rot * (quad[3].position)));
+
+//			var matrix = t.localToWorldMatrix.inverse
+//				* Matrix4x4.TRS (t.position, Quaternion.identity, t.lossyScale);
+
+//			var matrix = Matrix4x4.TRS (t.localPosition, t.localRotation, t.localScale).inverse
+//				* Matrix4x4.TRS (t.localPosition, Quaternion.identity, t.localScale);
+
+//			var matrix = Matrix4x4.TRS (new Vector3(0, -_rect.height-m_Spacing), Quaternion.identity, new Vector3(1,-1,1));
+
+
+			// Reverse quad index.
+//			quad[0].position = rot2*matrix.MultiplyPoint3x4(lm.MultiplyPoint3x4(rot3*lm.inverse.MultiplyPoint3x4(quad[0].position)));
+//			quad[1].position = rot2*matrix.MultiplyPoint3x4(lm.MultiplyPoint3x4(rot3*lm.inverse.MultiplyPoint3x4(quad[1].position)));
+//			quad[2].position = rot2*matrix.MultiplyPoint3x4(lm.MultiplyPoint3x4(rot3*lm.inverse.MultiplyPoint3x4(quad[2].position)));
+//			quad[3].position = rot2*matrix.MultiplyPoint3x4(lm.MultiplyPoint3x4(rot3*lm.inverse.MultiplyPoint3x4(quad[3].position)));
+
+			matrix =Matrix4x4.TRS (new Vector3(0, -_rect.height-m_Spacing), Quaternion.identity, new Vector3(1,-1,1));
+
+			var vecs = new Vector3[4];
+			t.GetWorldCorners (vecs);
+			var worldY = t.position.y - t.rect.height * t.lossyScale.y * t.pivot.y * 2;
+
+
+			var scale = t.lossyScale;
+			scale.y *= -1;
+			var eul = -t.rotation.eulerAngles;
+			eul.y *= -1;
+
+			matrix = Matrix4x4.identity
+				*
+				Matrix4x4.TRS (t.position, t.rotation, t.lossyScale).inverse
+				*
+				Matrix4x4.TRS (new Vector3(t.position.x, worldY, t.position.z), Quaternion.Euler (eul), scale)
+				;
+			
+
+
+			var lwm = t.localToWorldMatrix;
+			var wlm = t.worldToLocalMatrix;
+
+//			// Add reflected quad.
+//			UIVertexUtil.AddQuadToStream(quad, result);
+//			return;
+
+			var minY = Mathf.Min (
+				Mathf.Min (quad[0].position.y, quad[1].position.y),
+				Mathf.Min (quad[2].position.y, quad[3].position.y)
+			);
 
 			// Reflection is unnecessary.
 			if (m_Height < (v0.position.y - _rect.yMin) && m_Height < (v3.position.y - _rect.yMin))
@@ -167,16 +249,22 @@ namespace Coffee.UIExtensions
 			}
 
 			// Calculate reflected position and color.
-			MirrorReflectVertex(ref v0);
-			MirrorReflectVertex(ref v1);
-			MirrorReflectVertex(ref v2);
-			MirrorReflectVertex(ref v3);
+//			MirrorReflectVertex(ref v0);
+//			MirrorReflectVertex(ref v1);
+//			MirrorReflectVertex(ref v2);
+//			MirrorReflectVertex(ref v3);
 
 			// Reverse quad index.
 			quad[0] = v1;
 			quad[1] = v0;
 			quad[2] = v3;
 			quad[3] = v2;
+
+
+			quad[0].position = (matrix.MultiplyPoint3x4((quad[0].position)));
+			quad[1].position = (matrix.MultiplyPoint3x4((quad[1].position)));
+			quad[2].position = (matrix.MultiplyPoint3x4((quad[2].position)));
+			quad[3].position = (matrix.MultiplyPoint3x4((quad[3].position)));
 
 			// Add reflected quad.
 			UIVertexUtil.AddQuadToStream(quad, result);
@@ -195,11 +283,11 @@ namespace Coffee.UIExtensions
 			// Reflected color
 			var factor = Mathf.Clamp01((pos.y - _rect.yMin) / m_Height);
 			col *= Color.Lerp(m_StartColor, m_EndColor, factor);
+			vt.color = col;
 
 			// Reflected position.
-			pos.y = _rect.yMin * 2 - m_Spacing - pos.y;
-			vt.position = pos;
-			vt.color = col;
+//			pos.y = _rect.yMin * 2 - m_Spacing - pos.y;
+//			vt.position = pos;
 		}
 
 
